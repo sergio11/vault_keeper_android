3dfaa2d68943dcd20e0235a37e2f82ded46eeb0b
Sergio Sánchez <sss4esob@gmail.com>
2024-06-22 13:21:25 +0200

    Implement several improvements in account password row component

--- app/src/main/java/com/dreamsoftware/vaultkeeper/ui/features/home/components/AccountPasswordRow.kt
+++ app/src/main/java/com/dreamsoftware/vaultkeeper/ui/features/home/components/AccountPasswordRow.kt
@@ -1,30 +1,33 @@
 package com.dreamsoftware.vaultkeeper.ui.features.home.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.dreamsoftware.brownie.component.BrownieCardRow
 import com.dreamsoftware.brownie.component.BrownieDropdownMenuButton
 import com.dreamsoftware.brownie.component.BrownieDropdownMenuButtonItem
 import com.dreamsoftware.brownie.component.BrownieIconButton
+import com.dreamsoftware.brownie.component.BrownieImageIcon
+import com.dreamsoftware.brownie.component.BrownieImageSize
 import com.dreamsoftware.brownie.component.BrownieText
 import com.dreamsoftware.brownie.component.BrownieTextTypeEnum
 import com.dreamsoftware.brownie.utils.clickWithRipple
 import com.dreamsoftware.vaultkeeper.R
 import com.dreamsoftware.vaultkeeper.domain.model.AccountPasswordBO
 import com.dreamsoftware.vaultkeeper.ui.features.home.HomeScreenActionListener
-import com.dreamsoftware.vaultkeeper.utils.suggestionsWithImages
+import com.dreamsoftware.vaultkeeper.ui.utils.toAccountProviderBrush
+import com.dreamsoftware.vaultkeeper.ui.utils.toAccountProviderImage
 
 private const val EDIT_ACCOUNT_PASSWORD_ITEM_ID = "EDIT_ACCOUNT_PASSWORD"
 private const val DELETE_ACCOUNT_PASSWORD_ITEM_ID = "DELETE_ACCOUNT_PASSWORD"
@@ -35,24 +38,19 @@ fun AccountPasswordRow(
     actionListener: HomeScreenActionListener
 ) {
     with(MaterialTheme.colorScheme) {
+        val context = LocalContext.current
+        val accountPasswordColorBrush by rememberUpdatedState(account.accountName.toAccountProviderBrush(context))
         BrownieCardRow(
+            contentBrush = accountPasswordColorBrush,
             modifier = Modifier.clickWithRipple {
                 actionListener.onAccountPasswordClicked(account.uid)
             }
         ) {
-            val matchingImage =
-                suggestionsWithImages.firstOrNull { it.first == account.accountName }?.second
-
-            val painter = matchingImage ?: R.drawable.icon_others
-
-            Image(
-                painter = painterResource(painter),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(42.dp)
-                    .clip(CircleShape)
+            BrownieImageIcon(
+                iconRes = account.accountName.toAccountProviderImage(context),
+                size = BrownieImageSize.LARGE,
+                tintColor = Color.Unspecified
             )
-
             Column(
                 modifier = Modifier
                     .padding(start = 14.dp)
