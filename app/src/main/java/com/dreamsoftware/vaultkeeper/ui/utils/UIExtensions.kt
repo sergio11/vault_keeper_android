5e6f9659293f7fb7ebf99be2e204c20a26c5691c
Sergio Sánchez <sss4esob@gmail.com>
2024-06-22 13:21:25 +0200

    Implement several improvements in account password row component

--- app/src/main/java/com/dreamsoftware/vaultkeeper/ui/utils/UIExtensions.kt
+++ app/src/main/java/com/dreamsoftware/vaultkeeper/ui/utils/UIExtensions.kt
@@ -123,6 +123,36 @@ fun CardProviderEnum?.toCardProviderImage() = this?.let {
     }
 } ?: R.drawable.icon_card
 
+fun String?.toAccountProviderImage(context: Context) = this?.let {
+    with(context) {
+        when {
+            containsIgnoreCase(getString(R.string.amazon_prime)) -> R.drawable.icon_amazon_prime_video
+            containsIgnoreCase(getString(R.string.behance)) -> R.drawable.icon_behance
+            containsIgnoreCase(getString(R.string.discord)) -> R.drawable.icon_discord
+            containsIgnoreCase(getString(R.string.dribbble)) -> R.drawable.icon_dribbble
+            containsIgnoreCase(getString(R.string.facebook)) -> R.drawable.icon_facebook
+            containsIgnoreCase(getString(R.string.github)) -> R.drawable.icon_github
+            containsIgnoreCase(getString(R.string.gmail)) -> R.drawable.icon_gmail
+            containsIgnoreCase(getString(R.string.instagram)) -> R.drawable.icon_instagram
+            containsIgnoreCase(getString(R.string.linkedin)) -> R.drawable.icon_linkedin
+            containsIgnoreCase(getString(R.string.medium)) -> R.drawable.icon_medium
+            containsIgnoreCase(getString(R.string.messenger)) -> R.drawable.icon_messenger
+            containsIgnoreCase(getString(R.string.netflix)) -> R.drawable.icon_netflix
+            containsIgnoreCase(getString(R.string.pinterest)) -> R.drawable.icon_pinterest
+            containsIgnoreCase(getString(R.string.quora)) -> R.drawable.icon_quora
+            containsIgnoreCase(getString(R.string.reddit)) -> R.drawable.icon_reddit
+            containsIgnoreCase(getString(R.string.snapchat)) -> R.drawable.icon_snapchat
+            containsIgnoreCase(getString(R.string.spotify)) -> R.drawable.icon_spotify
+            containsIgnoreCase(getString(R.string.stackoverflow)) -> R.drawable.icon_stackoverflow
+            containsIgnoreCase(getString(R.string.tumblr)) -> R.drawable.icon_tumblr
+            containsIgnoreCase(getString(R.string.twitter)) -> R.drawable.icon_twitterx
+            containsIgnoreCase(getString(R.string.whatsapp)) -> R.drawable.icon_whatsapp
+            containsIgnoreCase(getString(R.string.wordpress)) -> R.drawable.icon_wordpress
+            containsIgnoreCase(getString(R.string.youtube)) -> R.drawable.icon_youtube
+            else -> R.drawable.icon_others
+        }
+    }
+} ?: R.drawable.icon_others
 
 fun CardProviderEnum.toCardProviderBrush(context: Context): Brush {
     val (startColorRes, endColorRes) = when (this) {
@@ -137,6 +167,45 @@ fun CardProviderEnum.toCardProviderBrush(context: Context): Brush {
     val endColor = ContextCompat.getColor(context, endColorRes)
     return Brush.verticalGradient(listOf(Color(startColor), Color(endColor)))
 }
+
+fun String?.toAccountProviderBrush(context: Context): Brush = this?.let {
+    val (startColorRes, endColorRes) = with(context) {
+        when {
+            containsIgnoreCase(getString(R.string.amazon_prime)) -> R.color.amazon_prime_start to R.color.amazon_prime_end
+            containsIgnoreCase(getString(R.string.behance)) -> R.color.behance_start to R.color.behance_end
+            containsIgnoreCase(getString(R.string.discord)) -> R.color.discord_start to R.color.discord_end
+            containsIgnoreCase(getString(R.string.dribbble)) -> R.color.dribbble_start to R.color.dribbble_end
+            containsIgnoreCase(getString(R.string.facebook)) -> R.color.facebook_start to R.color.facebook_end
+            containsIgnoreCase(getString(R.string.github)) -> R.color.github_start to R.color.github_end
+            containsIgnoreCase(getString(R.string.gmail)) -> R.color.gmail_start to R.color.gmail_end
+            containsIgnoreCase(getString(R.string.instagram)) -> R.color.instagram_start to R.color.instagram_end
+            containsIgnoreCase(getString(R.string.linkedin)) -> R.color.linkedin_start to R.color.linkedin_end
+            containsIgnoreCase(getString(R.string.medium)) -> R.color.medium_start to R.color.medium_end
+            containsIgnoreCase(getString(R.string.messenger)) -> R.color.messenger_start to R.color.messenger_end
+            containsIgnoreCase(getString(R.string.netflix)) -> R.color.netflix_start to R.color.netflix_end
+            containsIgnoreCase(getString(R.string.pinterest)) -> R.color.pinterest_start to R.color.pinterest_end
+            containsIgnoreCase(getString(R.string.quora)) -> R.color.quora_start to R.color.quora_end
+            containsIgnoreCase(getString(R.string.reddit)) -> R.color.reddit_start to R.color.reddit_end
+            containsIgnoreCase(getString(R.string.snapchat)) -> R.color.snapchat_start to R.color.snapchat_end
+            containsIgnoreCase(getString(R.string.spotify)) -> R.color.spotify_start to R.color.spotify_end
+            containsIgnoreCase(getString(R.string.stackoverflow)) -> R.color.stackoverflow_start to R.color.stackoverflow_end
+            containsIgnoreCase(getString(R.string.tumblr)) -> R.color.tumblr_start to R.color.tumblr_end
+            containsIgnoreCase(getString(R.string.twitter)) -> R.color.twitter_start to R.color.twitter_end
+            containsIgnoreCase(getString(R.string.whatsapp)) -> R.color.whatsapp_start to R.color.whatsapp_end
+            containsIgnoreCase(getString(R.string.wordpress)) -> R.color.wordpress_start to R.color.wordpress_end
+            containsIgnoreCase(getString(R.string.youtube)) -> R.color.youtube_start to R.color.youtube_end
+            else -> R.color.others_start to R.color.others_end
+        }
+    }
+    val startColor = ContextCompat.getColor(context, startColorRes)
+    val endColor = ContextCompat.getColor(context, endColorRes)
+    Brush.verticalGradient(listOf(Color(startColor), Color(endColor)))
+} ?: Brush.verticalGradient(
+    colors = listOf(
+        Color(ContextCompat.getColor(context, R.color.others_start)),
+        Color(ContextCompat.getColor(context, R.color.others_end))
+    )
+)
 
 fun String?.toCardProviderImage() = this?.takeIf { CardProviderEnum.entries.map(CardProviderEnum::name).contains(it) }?.let {
     CardProviderEnum.valueOf(this).toCardProviderImage()
